388d554cceb69e97cee7f295ed2a28af881edc5e
takamasa hirano <pingyexiaoshang@gmail.com>
2024-01-31 10:49:15 +0900

    スライド追加

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+
+$('.fade').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear'
+    });
