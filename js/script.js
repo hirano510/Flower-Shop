fcf25e052a60ef25f5337249107004710a00ef6b
takamasa hirano <pingyexiaoshang@gmail.com>
2024-01-31 17:24:05 +0900

    タイトル装飾完了

--- js/script.js
+++ js/script.js
@@ -1,13 +1,16 @@
-
-
 $('.fade').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 1000,
+    autoplaySpeed: 3000,
     dots: true,
     infinite: true,
     speed: 500,
     fade: true,
     cssEase: 'linear'
     });
+
+window.addEventListener('load', () => {
+    AOS.init();
+});
+    
